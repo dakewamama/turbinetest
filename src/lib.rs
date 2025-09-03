87f90d30b7b88d8e0ba12a3e9003a0fc9b4394d5
dakewamama <davidasiadiachi@gmail.com>
2025-09-03 08:23:38 +0000

    tx code complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 #[cfg(test)]
 mod tests {
     use solana_sdk::{
-        signature::{Keypair, Signer, read_keypair_file},
-        pubkey::Pubkey,
+    signature::{Keypair, Signer, read_keypair_file},
+    transaction::Transaction,
     };
     use bs58;
-    use std::io::{self, BufRead};
     use solana_client::rpc_client::RpcClient;
+    use solana_program::system_instruction::transfer;
+    use solana_sdk::pubkey::Pubkey;
+    use std::str::FromStr;
+    use solana_sdk::hash::hash;
+
 
     #[test]
     fn keygen() {
@@ -64,6 +68,41 @@ mod tests {
 
     #[test]
     fn transfer_sol() {
-        
+       // find  wallet 
+       let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+       // Generate a signature from the keypair
+       let pubkey = keypair.pubkey();
+
+       let message_bytes = b"I verify my Solana Keypair!";
+       let sig = keypair.sign_message(message_bytes);
+       let sig_hashed = hash(sig.as_ref()); 
+
+       // Verify the signature using the public key
+        match sig.verify(&pubkey.to_bytes(), &sig_hashed.to_bytes()) {
+            true => println!("Signature verified"),
+            false => println!("Verification failed"),
+        }
+        let to_pubkey = Pubkey::from_str("Cf27GhB8m4q1p9RK8e5MYgmuGQZQngbBguAyKJWpTEKZ").unwrap();
+        // Connect to devnet
+        const RPC_URL: &str = "https://api.devnet.solana.com";
+
+        let rpc_client = RpcClient::new(RPC_URL);
+        // Fetch recent blockhash
+        let recent_blockhash = rpc_client
+            .get_latest_blockhash()
+            .expect("Failed to get recent blockhash");
+        let transaction = Transaction::new_signed_with_payer(
+        &[transfer(&keypair.pubkey(), &to_pubkey, 1_000_000)],
+        Some(&keypair.pubkey()),
+        &vec![&keypair],
+        recent_blockhash,
+        );
+        let signature = rpc_client
+            .send_and_confirm_transaction(&transaction)
+            .expect("Failed to send transaction");
+        println!(
+            "Success! Check out your TX here: https://explorer.solana.com/tx/{}/?cluster=devnet",
+            signature
+        );
     }
 }
